c6019956fa84c6ad01b148ffc235ca79a92018c2
wang_wei <wang_wei05@uisftech.com>
2016-12-14 15:08:45 +0800

    集成swagger2

--- spring-boot-demo-V2/src/main/java/com/uisftech/Application.java
+++ spring-boot-demo-V2/src/main/java/com/uisftech/Application.java
@@ -1,9 +1,18 @@
 package com.uisftech;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.uisftech.cloan.preloan.domain.User;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 @EnableAutoConfiguration
@@ -13,7 +22,6 @@ public class Application {
 	String index() {
 		return "www.yoodb.com";
 	}
-
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
--- spring-boot-demo-V2/src/main/java/com/uisftech/cloan/preloan/web/UserController.java
+++ spring-boot-demo-V2/src/main/java/com/uisftech/cloan/preloan/web/UserController.java
@@ -1,7 +1,19 @@
 package com.uisftech.cloan.preloan.web;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +22,17 @@ import com.uisftech.cloan.preloan.domain.User;
 import com.uisftech.cloan.preloan.service.IUserService;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
 
 	@Autowired
 	private IUserService userService;
 
+	@ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "String")
 	@RequestMapping(value="/findUser/{id}",method=RequestMethod.GET)
 	public User findUser(@PathVariable String id){
 		return userService.findUser(id);
 	}
+
 }
